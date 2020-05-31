02f13d0efd5d8bb7ea0f8e9a15dca3e6a308d9d1
kenta-s <knt01222@gmail.com>
2020-05-31 11:46:11 +0900

    update

--- canvasTest.js
+++ canvasTest.js
@@ -20,7 +20,44 @@ const drawTriangles = function() {
   ctx.fill();
 }
 
+const drawCircles = function() {
+  const canvas = document.getElementById('canvas-3');
+  const ctx = canvas.getContext('2d');
+
+  ctx.beginPath();
+  ctx.arc(75, 75, 50, 0, Math.PI * 2, true); // 外の円
+  ctx.moveTo(110, 75);
+  ctx.arc(75, 75, 35, 0, Math.PI, false);  // 口 (時計回り)
+  ctx.moveTo(65, 65);
+  ctx.arc(60, 65, 5, 0, Math.PI * 2, true);  // 左目
+  ctx.moveTo(95, 65);
+  ctx.arc(90, 65, 5, 0, Math.PI * 2, true);  // 右目
+  ctx.stroke();
+}
+
+const drawLines = function() {
+  const canvas = document.getElementById('canvas-4');
+  const ctx = canvas.getContext('2d');
+
+  // Filled triangle
+  ctx.beginPath();
+  ctx.moveTo(25, 25);
+  ctx.lineTo(105, 25);
+  ctx.lineTo(25, 105);
+  ctx.fill();
+
+  // Stroked triangle
+  ctx.beginPath();
+  ctx.moveTo(125, 125);
+  ctx.lineTo(125, 45);
+  ctx.lineTo(45, 125);
+  ctx.closePath();
+  ctx.stroke();
+}
+
 document.addEventListener("DOMContentLoaded", function(){
   drawRectangles();
   drawTriangles();
+  drawCircles();
+  drawLines();
 });
